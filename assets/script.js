c7c8124c7af5add83939fcfceeedd7bbb4375fb7
Bryan <bryantdalton19@gmail.com>
2022-09-15 12:32:31 -0400

    expanded list to 12items, added live timer, added timeline aside

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,12 @@ var dateEl = $('#date')
 var timeEl = $('#time')
 
 dateEl.text(moment().format('dddd MMMM YYYY'))
-timeEl.text(moment().format('h:mm:ss a'))
+// timeEl.text(moment().format('h:mm:ss a'))
+function displayTime () {
+  var now = moment().format('h:mm:ss a')
+  timeEl.text(now)
+}
+setInterval(displayTime, 1000)
 
 $( function() {
     $( "#sortable" ).sortable();
