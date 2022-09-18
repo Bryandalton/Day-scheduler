6ba04859c9ff9ec7db1773153107fb6693357db8
[Bryan Dalton] <[bryantdalton19@gmail.com]>
2022-09-17 21:55:57 -0400

    Fixed time based color coding

--- assets/script.js
+++ assets/script.js
@@ -1,46 +1,45 @@
 var dateEl = $('#date')
 var timeEl = $('#time')
 var present = moment().hours();
-var scheduleTime = [8,9,10,11,12,13,14,15,26,17,18,19,20]
+var scheduleTime = [8,9,10,11,12,13,14,15,16,17,18,19,20]
 var scheduleItem = $('.schedule-item')
 
-dateEl.text(moment().format('dddd MMMM YYYY'))
+dateEl.text(moment().format('dddd, MMMM YYYY'))
 // timeEl.text(moment().format('h:mm:ss a'))
 function displayTime () {
   var now = moment().format('h:mm:ss a')
   timeEl.text(now)
-}
-setInterval(displayTime, 1000)
+};
+setInterval(displayTime, 1000);
 
 $( function() {
     $( "#sortable" ).sortable();
   } );
 
-  $( function () {
-
-  })
 
 
 console.log(present)
 console.log(scheduleTime[0])
 console.log(scheduleTime[1])
+// console.log(scheduleItem)
+console.log(scheduleItem[8])
+console.log(scheduleItem[1])
 
-console.log(scheduleItem)
 
-for (let i = 0; i > scheduleTime.length; i++) {
-  scheduleTime[i] = scheduleItem
+
+for (let i = 0; i < scheduleTime.length; i++) {
 
   if (present == scheduleTime[i]) {
-    scheduleItem.addClass('present')
-    scheduleItem.removeClass('past')
-    scheduleItem.removeClass('future')
+    scheduleItem.eq(i).removeClass('past')
+    scheduleItem.eq(i).addClass('present')
+    scheduleItem.eq(i).removeClass('future')
    } else if (present > scheduleTime[i]) {
-    scheduleItem.addClass('past')
-    scheduleItem.removeClass('present')
-    scheduleItem.removeClass('future')
+    scheduleItem.eq(i).addClass('past')
+    scheduleItem.eq(i).removeClass('present')
+    scheduleItem.eq(i).removeClass('future')
    } else if (present < scheduleTime[i]) {
-    scheduleItem.addClass('future')
-    scheduleItem.removeClass('past')
-    scheduleItem.removeClass('present')
+    scheduleItem.eq(i).addClass('future')
+    scheduleItem.eq(i).removeClass('past')
+    scheduleItem.eq(i).removeClass('present')
   }
 }
