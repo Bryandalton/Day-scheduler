9a2ad22f98f9ee29aacdbb7df0cc0775ee4e5b5e
[Bryan Dalton] <[bryantdalton19@gmail.com]>
2022-09-18 20:14:40 -0400

    ui changes, save buttons added, and local storage added

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,12 @@
-var dateEl = $('#date')
-var timeEl = $('#time')
+var dateEl = $('#date');
+var timeEl = $('#time');
+var inputText = $('.input-text');
 var present = moment().hours();
-var scheduleTime = [8,9,10,11,12,13,14,15,16,17,18,19,20]
-var scheduleItem = $('.schedule-item')
+var scheduleTime = [8,9,10,11,12,13,14,15,16,17,18,19,20];
+var scheduleItem = $('.schedule-item');
+var saveBtn = $('.save');
 
-dateEl.text(moment().format('dddd, MMMM YYYY'))
+dateEl.text(moment().format('dddd, MMMM YYYY'));
 // timeEl.text(moment().format('h:mm:ss a'))
 function displayTime () {
   var now = moment().format('h:mm:ss a')
@@ -12,34 +14,34 @@ function displayTime () {
 };
 setInterval(displayTime, 1000);
 
-$( function() {
-    $( "#sortable" ).sortable();
-  } );
+function updateColor () {
+  for (let i = 0; i < scheduleTime.length; i++) {
 
+    if (present == scheduleTime[i]) {
+      scheduleItem.eq(i).removeClass('past')
+      scheduleItem.eq(i).addClass('present')
+      scheduleItem.eq(i).removeClass('future')
+     } else if (present > scheduleTime[i]) {
+      scheduleItem.eq(i).addClass('past')
+      scheduleItem.eq(i).removeClass('present')
+      scheduleItem.eq(i).removeClass('future')
+     } else if (present < scheduleTime[i]) {
+      scheduleItem.eq(i).addClass('future')
+      scheduleItem.eq(i).removeClass('past')
+      scheduleItem.eq(i).removeClass('present')
+    }
+  }
+};
 
+setInterval(updateColor, 1000)
+
+function saveEvent () {
+  var savedEvent = inputText.val().trim();
+  var saveConfirm = $('#confirm-save')
+  saveConfirm.text('Save Confrimed!')
+  localStorage.setItem('event',savedEvent)
+};
+
+saveBtn.on('click', saveEvent)
 
 console.log(present)
-console.log(scheduleTime[0])
-console.log(scheduleTime[1])
-// console.log(scheduleItem)
-console.log(scheduleItem[8])
-console.log(scheduleItem[1])
-
-
-
-for (let i = 0; i < scheduleTime.length; i++) {
-
-  if (present == scheduleTime[i]) {
-    scheduleItem.eq(i).removeClass('past')
-    scheduleItem.eq(i).addClass('present')
-    scheduleItem.eq(i).removeClass('future')
-   } else if (present > scheduleTime[i]) {
-    scheduleItem.eq(i).addClass('past')
-    scheduleItem.eq(i).removeClass('present')
-    scheduleItem.eq(i).removeClass('future')
-   } else if (present < scheduleTime[i]) {
-    scheduleItem.eq(i).addClass('future')
-    scheduleItem.eq(i).removeClass('past')
-    scheduleItem.eq(i).removeClass('present')
-  }
-}
