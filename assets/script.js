029ee925723387d56047dac55be535f4a0401032
[Bryan Dalton] <[bryantdalton19@gmail.com]>
2022-09-18 21:57:09 -0400

    fixed local storage issue and formatting

--- assets/script.js
+++ assets/script.js
@@ -1,60 +1,56 @@
-var dateEl = $('#date');
-var timeEl = $('#time');
-var inputText = $('.input-text');
+var dateEl = $("#date");
+var timeEl = $("#time");
+var inputText = $(".input-text");
 var present = moment().hours();
-var scheduleTime = [8,9,10,11,12,13,14,15,16,17,18,19,20];
-var scheduleItem = $('.schedule-item');
-var saveBtn = $('.save');
-var scheduleText = $('.schedule-text')
+var scheduleTime = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+var scheduleItem = $(".schedule-item");
+var saveBtn = $(".save");
+var scheduleText = $(".schedule-text");
 
-dateEl.text(moment().format('dddd, MMMM YYYY'));
-// timeEl.text(moment().format('h:mm:ss a'))
-function displayTime () {
-  var now = moment().format('h:mm:ss a')
-  timeEl.text(now)
-};
+dateEl.text(moment().format("dddd, MMMM YYYY"));
+function displayTime() {
+  var now = moment().format("h:mm:ss a");
+  timeEl.text(now);
+}
 setInterval(displayTime, 1000);
 
-function updateColor () {
+function updateColor() {
   for (let i = 0; i < scheduleTime.length; i++) {
-
     if (present == scheduleTime[i]) {
-      scheduleItem.eq(i).removeClass('past')
-      scheduleItem.eq(i).addClass('present')
-      scheduleItem.eq(i).removeClass('future')
-     } else if (present > scheduleTime[i]) {
-      scheduleItem.eq(i).addClass('past')
-      scheduleItem.eq(i).removeClass('present')
-      scheduleItem.eq(i).removeClass('future')
-     } else if (present < scheduleTime[i]) {
-      scheduleItem.eq(i).addClass('future')
-      scheduleItem.eq(i).removeClass('past')
-      scheduleItem.eq(i).removeClass('present')
+      scheduleItem.eq(i).removeClass("past");
+      scheduleItem.eq(i).addClass("present");
+      scheduleItem.eq(i).removeClass("future");
+    } else if (present > scheduleTime[i]) {
+      scheduleItem.eq(i).addClass("past");
+      scheduleItem.eq(i).removeClass("present");
+      scheduleItem.eq(i).removeClass("future");
+    } else if (present < scheduleTime[i]) {
+      scheduleItem.eq(i).addClass("future");
+      scheduleItem.eq(i).removeClass("past");
+      scheduleItem.eq(i).removeClass("present");
     }
   }
-};
+}
 //calls color code and checks for updates every minute
-updateColor()
-setInterval(updateColor, 60000)
+updateColor();
+setInterval(updateColor, 60000);
 
-function saveEvent (listIndex) {
+function saveEvent(listIndex) {
   var savedEvent = inputText.eq(listIndex).val().trim();
-  var saveConfirm = $('#confirm-save')
-  var savedItem = $('<p>')
-  var localStorageKey = 'event#' + listIndex;
-  saveConfirm.text('Save Confrimed!')
-  scheduleItem.eq(listIndex).append(savedItem)
-  localStorage.setItem(localStorageKey,savedEvent)
-  savedItem.text(localStorage.getItem(localStorageKey))
-};
+  var saveConfirm = $("#confirm-save");
+  var savedItem = $("<p>");
+  var localStorageKey = "event#" + listIndex;
+  saveConfirm.text("Save Confrimed!");
+  scheduleItem.eq(listIndex).append(savedItem);
+  localStorage.setItem(localStorageKey, savedEvent);
+  savedItem.text(localStorage.getItem(localStorageKey));
+}
 
 saveBtn.each((index, btn) => {
-  btn.addEventListener('click', () => saveEvent(index))
+  btn.addEventListener("click", () => saveEvent(index));
 });
 
 scheduleText.each((index, text) => {
-  var localStorageKey = 'event#' + index;
-  text.innerText = localStorage.getItem(localStorageKey)
-})
-
-console.log(present)
+  var localStorageKey = "event#" + index;
+  text.innerText = localStorage.getItem(localStorageKey);
+});
